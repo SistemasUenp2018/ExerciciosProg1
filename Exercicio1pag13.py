481d9a68c186976977b7156eb834e571c0e30502
sistemasuenp <si.uenp2018@gmail.com>
2018-03-21 18:50:26 -0300

    Exercício 1  da página 13 parte 2

--- Exercicio1pag13.py
+++ Exercicio1pag13.py
@@ -1,11 +1,15 @@
+import colorsys
 import turtle
 
 tata = turtle.Turtle()
 tata.shape("turtle")
+tata.fillcolor("orange")
+tata.fill("true")
 tata.forward(200) #O cursor turtle avanca 200 pixels"
 tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
 tata.forward(200) #O cursor turtle avanca 200 pixels
 tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
 tata.forward(200) #O cursor turtle avanca 200 pixels
 tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
+tata.fill("false")
 turtle.done()     #O cursos turtle para.
