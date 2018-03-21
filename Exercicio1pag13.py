7175da8a51d1974a5ea5b896a54a7e41181ab261
sistemasuenp <si.uenp2018@gmail.com>
2018-03-21 18:51:58 -0300

    Exercício 1  da página 13 parte 2.1

--- Exercicio1pag13.py
+++ Exercicio1pag13.py
@@ -1,15 +1,15 @@
 import colorsys
 import turtle
 
-tata = turtle.Turtle()
-tata.shape("turtle")
-tata.fillcolor("orange")
-tata.fill("true")
+tata = turtle.Turtle() #Faz a instância da classe turtle
+tata.shape("turtle") #Escolhe o formato do cursor
+tata.fillcolor("orange") #Escolhe a cor para preencher o desenho
+tata.fill("true") #Muda o "status" colorido para verdadeiro
 tata.forward(200) #O cursor turtle avanca 200 pixels"
 tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
 tata.forward(200) #O cursor turtle avanca 200 pixels
 tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
 tata.forward(200) #O cursor turtle avanca 200 pixels
 tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
-tata.fill("false")
+tata.fill("false") #Muda o "status" colorido para falso
 turtle.done()     #O cursos turtle para.
