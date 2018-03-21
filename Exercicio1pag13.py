f5d556c0a2358dec28f795e7be0da92e346d9802
sistemasuenp <si.uenp2018@gmail.com>
2018-03-21 18:39:41 -0300

    Exercício 1  da página 13 comentado

---
+++ Exercicio1pag13.py
@@ -0,0 +1,11 @@
+import turtle
+
+tata = turtle.Turtle()
+tata.shape("turtle")
+tata.forward(200) #O cursor turtle avanca 200 pixels"
+tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
+tata.forward(200) #O cursor turtle avanca 200 pixels
+tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
+tata.forward(200) #O cursor turtle avanca 200 pixels
+tata.left(120)    #O cursos turtle faz uma curva aguda de 120 graus, deixando um angulo de 60 graus
+turtle.done()     #O cursos turtle para.
