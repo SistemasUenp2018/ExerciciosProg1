b23a86e963ed43a404668131c55069e00a3d2940
Marcelo Lemes <lemmarcelo@gmail.com>
2018-03-22 20:21:18 -0300

    Update Exercicio 12.py

--- Exercicios pag 19/Exercicio 12.py
+++ Exercicios pag 19/Exercicio 12.py
@@ -1,1 +1,5 @@
 
+distancia = float(input("Digite o a distancia percorrida: "))
+combustivel = float(input("Digite o combustivel gasto: "))
+resultado = distancia/combustivel
+print("A media de combustivel gasto eh: %s litros" % resultado)
