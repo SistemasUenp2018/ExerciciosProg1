51d45fb6039eae6aee8cd1c17da508b98607f024
Marcelo Lemes <lemmarcelo@gmail.com>
2018-03-22 19:54:52 -0300

    Create Exercicio 13.py

---
+++ Exercicios pag 19/Exercicio 13.py
@@ -0,0 +1,3 @@
+produto = float(input("Digite o valor do produto: ")) #Recebendo o valor do produto
+prestacao = produto/5 #Fazendo o calculo da prestacao sem juros
+print("O valor da prestacao serah: ", prestacao) #exibindo o valor da prestacao
