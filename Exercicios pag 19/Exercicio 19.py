40918015e2eed12716c53f1cfa59650f52146453
sistemasuenp <si.uenp2018@gmail.com>
2018-03-22 18:47:10 -0300

    Ex 19 pag 20

---
+++ Exercicios pag 19/Exercicio 19.py
@@ -0,0 +1,3 @@
+n1 = float(input("Digita a area a ser pintada ")) #Recebe a area a ser pintada
+n1= n1/3 #Cada lata da pra 3 metros de area pintada, entao dividindo por 3 descobrimos quantas latas serao necessarias
+print("Serao necessarias %s latas de tinta, e o preco final serah de R$%s"% (n1,n1*80)) #exibe as latas necessarias, e multiplica por 80 que eh o valor unitario da lata
