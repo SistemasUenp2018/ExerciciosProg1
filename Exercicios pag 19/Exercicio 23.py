15def5d332c8e33676766e7112f8ac153c30af12
Marcelo Lemes <lemmarcelo@gmail.com>
2018-03-22 20:59:35 -0300

    Create Exercicio 23.py

---
+++ Exercicios pag 19/Exercicio 23.py
@@ -0,0 +1,3 @@
+valorPremio = float(input("Digite o valor do premio em reais: "))
+qtdAmigos = int(input("Digite a quantidade de amigos que irao dividir: "))
+print("O premio serah R$%s para cada amigo" % (valorPremio/qtdAmigos))
