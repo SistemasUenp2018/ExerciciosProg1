9cc7de01792e32643f66764abe795f7b630338b4
sistemasuenp <si.uenp2018@gmail.com>
2018-03-21 23:41:07 -0300

    Ex 1 pag 19

---
+++ Exercicios pag 19/Exercicio 1.py
@@ -0,0 +1,5 @@
+media = 0 #a variavel que vai guardar o valor desejado
+media = input("Digite o valor do primeiro numero: ") #Recebendo o valor do primeiro numero na variavel
+media = media + (input("Digite o valor do segundo numero: ")) #Atribuindo na variavel o valor que ela possuia anteriormente, somado ao segundo numero
+media = (media/2) #os numeros jah estavam somados, entao agorah eh so dividir os dois numeros para obter a media
+print("O valor da media eh: ", media) #exibindo a media
