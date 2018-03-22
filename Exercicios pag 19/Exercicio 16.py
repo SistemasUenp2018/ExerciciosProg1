ef1e481dca6feac6115179ec30555c1223d3b994
Marcelo Lemes <lemmarcelo@gmail.com>
2018-03-22 19:31:47 -0300

    Create Exercicio 16.py
    Exercicio 16 pag 19

---
+++ Exercicios pag 19/Exercicio 16.py
@@ -0,0 +1,5 @@
+qtdeHoras = float(input("Digite a quantidade de horas trabalhadas: "))  #Recebe a quantidade de horas
+valorHora = float(input("Digite o valor da hora trabalhada: ")) #Recebe o valor da hora
+resultado = qtdeHoras * valorHora #Faz a multiplicacao da hora pelo valor
+
+print("O valor do salario sera ",resultado) #exibe o resultado
