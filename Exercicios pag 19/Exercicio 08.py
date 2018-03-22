8775060f5814501a534ed3a9bcb58dd4a8abecf3
Marcelo Lemes <lemmarcelo@gmail.com>
2018-03-22 20:50:01 -0300

    Rename Exercicio 8.py to Exercicio 08.py

---
+++ Exercicios pag 19/Exercicio 08.py
@@ -0,0 +1,2 @@
+numero = int(input("Qual a sua carga horaria da disciplina? "))
+print ("Voce pode ter %s faltas neste periodo letivo"%int(numero*0.25))
--- Exercicios pag 19/Exercicio 8.py
+++
@@ -1,2 +0,0 @@
-numero = int(input("Qual a sua carga horaria da disciplina? "))
-print ("Voce pode ter %s faltas neste periodo letivo"%int(numero*0.25))
