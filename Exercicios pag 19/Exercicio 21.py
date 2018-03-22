01387b3da8819dca09cf96edb1ccfb4721b5458d
Marcelo Lemes <lemmarcelo@gmail.com>
2018-03-22 20:56:16 -0300

    Create Exercicio 21.py

---
+++ Exercicios pag 19/Exercicio 21.py
@@ -0,0 +1,4 @@
+n1= int(input("Digite a area da sala em metros cubicos: "))
+n2= int(input("Digite a area do taco em metros cubicos: "))
+resultado = n1/n2
+print("Serao necessarios %s tacos para preencher a sala"%(resultado))
