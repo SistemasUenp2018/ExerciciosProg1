c500e41b06ba7bd4169065213c73db746256b054
sistemasuenp <si.uenp2018@gmail.com>
2018-03-22 23:41:24 -0300

    Exercicio 2 pag 26 Comentado

--- Exercicios pag 26/Exercicio 02.py
+++ Exercicios pag 26/Exercicio 02.py
@@ -1,8 +1,7 @@
-consumo = int(input("Digite em metros cubicos qual o consumo de agua mensal: "))
-
-if consumo <=10:
-    print(" Seu consumo foi: ",consumo*2.55)
-elif consumo <= 20:
-    print(" Seu consumo foi: ", consumo * 3.44)
-else:
+consumo = int(input("Digite em metros cubicos qual o consumo de agua mensal: ")) #Recebe o consumo
+if consumo <=10: #Compara se oonsumo eh menor ou igual a 10
+    print(" Seu consumo foi: ",consumo*2.55) #Exibe o consumo com a aliquota correspondente
+elif consumo <= 20: #Compara se o consumo eh menor ou igual a 20
+    print(" Seu consumo foi: ", consumo * 3.44)  #Exibe o consumo com a aliquota correspondente
+else: #Se o consumo nao for menor ou igual a 10 ou 20, entao eh a terceira aliquota a aplicada
     print("Seu consumo foi: ", consumo * 5.25)
