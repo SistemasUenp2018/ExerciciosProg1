87f1623d68d2b3598df300e82f349503366a8de7
marcelo <lemmarcelo@gmail.com>
2018-03-26 17:48:04 -0300

    Exercicio 06 comentado

---
+++ Exercicios pag 26/Exercicio 06.py
@@ -0,0 +1,3 @@
+raio = int(input("Insira o valor do raio da esfera: ")) # Inserindo o valor do RAIO
+volume = (4/3)*3.14*raio**3 #Aplicando a formula passada na apostila
+print("Exiba o volume da esfera ",volume) #Exibindo o resultado
