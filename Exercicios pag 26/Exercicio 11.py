1bb917407eb061503c1c6b745c7a0a1210f2f11c
marcelo <lemmarcelo@gmail.com>
2018-03-29 22:54:00 -0300

    Exercicio 11

---
+++ Exercicios pag 26/Exercicio 11.py
@@ -0,0 +1,14 @@
+a = int(input("Digite o codigo da regiao: "))
+
+if a == 1:
+    print("Sul")
+elif a == 2:
+    print("Sudeste")
+elif a == 3:
+    print("Centro Oeste")
+elif a == 4:
+    print("Nordeste")
+elif a == 5:
+    print("Norte")
+else:
+    print("Regiao nao encontrada")
