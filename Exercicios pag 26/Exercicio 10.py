7e2444f4d4327e5ff0474647af1aba78a7ee493b
marcelo <lemmarcelo@gmail.com>
2018-03-29 22:49:41 -0300

    Exercicio 10

---
+++ Exercicios pag 26/Exercicio 10.py
@@ -0,0 +1,2 @@
+qtdAulas = int(input("Quantidade de aulas totais da disciplina: "))
+print("Voce pode ter no máximo %s faltas "% int(qtdAulas*0.25))
--- Limpo.py
+++
@@ -1,4 +0,0 @@
-a = float(input())
-b = float(input())
-MEDIA = (a*3.5+a*7.5)/11
-print("PROD = %s"%MEDIA)
