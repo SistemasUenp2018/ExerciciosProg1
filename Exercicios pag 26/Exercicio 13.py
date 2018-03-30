2aaf195039c44b11416ab8a3c67f7935cd248068
marcelo <lemmarcelo@gmail.com>
2018-03-29 23:33:17 -0300

    Exercicio 13

---
+++ Exercicios pag 26/Exercicio 13.py
@@ -0,0 +1,8 @@
+nota1 = float (input("Digite a nota 1: "))
+nota2 = float (input("Digite a nota 2: "))
+media = (nota1+nota2)/2
+
+if media >= 7:
+    print("Parabens, aprovado")
+else:
+    print("Que pena, reprovado")
