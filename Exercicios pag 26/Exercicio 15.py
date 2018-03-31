8185fa0d913a51af0d8edd08d9d041cb0f3e7750
marcelo <lemmarcelo@gmail.com>
2018-03-31 16:24:27 -0300

    Exercicio 15

---
+++ Exercicios pag 26/Exercicio 15.py
@@ -0,0 +1,12 @@
+condicaoPagamento = int(input("Insira por favor, o codigo da condicao de pagamento: "))
+precoVenda = float(input("Digite o valor total da venda: "))
+
+if condicaoPagamento == 3:
+    print("O valor da venda eh: R$ %.2f"%(precoVenda))
+
+elif condicaoPagamento == 2:
+    print("O valor da venda eh: R$ %.2f"%(precoVenda*0.95))
+
+elif condicaoPagamento == 1:
+    print("O valor da venda eh: R$ %.2f"%(precoVenda*0.90))
+
