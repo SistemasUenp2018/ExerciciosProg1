824c96f29e2cbcff04f868ead913c7c7ddf4e510
marcelo <lemmarcelo@gmail.com>
2018-04-01 00:07:00 -0300

    Exercicio 17

---
+++ Exercicios pag 26/Exercicio 17.py
@@ -0,0 +1,6 @@
+letra = input("Digite apenas uma letra: ")
+
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u" or letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U":
+    print("Uma vogal foi digitada")
+else:
+    print("Uma consoante foi digitada")
