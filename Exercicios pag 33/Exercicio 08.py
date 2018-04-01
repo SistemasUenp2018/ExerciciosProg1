843321f3f7eca9f4f6b737e53e3763184c64160a
marcelo <lemmarcelo@gmail.com>
2018-04-01 02:30:06 -0300

    Exercicio 08

---
+++ Exercicios pag 33/Exercicio 08.py
@@ -0,0 +1,20 @@
+num = -2
+
+while num < 23:
+    num +=5
+    print(num)
+
+num = 26
+
+print("______________________________")
+while num > -10:
+    num-=6
+    print(num)
+
+print("______________________________")
+
+num = 11
+
+while num < 51:
+    num +=8
+    print(num)
